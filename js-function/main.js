05513b42d16c745fe6294f1e891a2ed921d2791f
Anusree Melath <anusree450m@gmail.com>
2024-06-21 19:24:35 +0530

    java script function

---
+++ js-function/main.js
@@ -0,0 +1,12 @@
+function makeAteaorcofee(powder){
+   console.log("step1: boil the water");
+   console.log("step1: add milk");
+   console.log(`step1 put ${powder}  powder`);
+   console.log("step3: boil it");
+   console.log("step1: sreve it into glass");
+}
+
+makeAteaorcofee("cofee");
+makeAteaorcofee("tea");
+
+
