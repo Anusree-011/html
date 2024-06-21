4a5d953f8bd2e422e3276331f639c181deaf71d7
Anusree Melath <anusree450m@gmail.com>
2024-06-21 18:35:07 +0530

    first project commit

---
+++ js-project1/main.js
@@ -0,0 +1,9 @@
+let name="Anusree";
+let result="Anusree";
+
+let heading= document.querySelector(".heading");
+console.log(heading)
+heading.innerText=(`My name is ${result}`);
+console.log("Anusree");
+let currypowder=["salt","chilli","turmeric"];
+console.log(currypowder [2]);
