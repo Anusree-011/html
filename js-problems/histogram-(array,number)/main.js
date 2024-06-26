d1c4caebe9d976d2bf31b042e4f1fc50eb9205f3
Anusree Melath <anusree450m@gmail.com>
2024-06-26 10:36:53 +0530

    histogram array and number reperesent

---
+++ js-problems/histogram-(array,number)/main.js
@@ -0,0 +1,15 @@
+let histogram =(arr,char)=>{
+    arr.map((number)=>console.log(char.repeat(number)))
+    //let charRepeat=''
+    //for(let i=0;i<arr.length;i++){
+        
+// for(let j=1;j<=arr[i];j++){
+        
+        
+          //  charRepeat+=char
+      //  }
+       // console.log(charRepeat)
+        //charRepeat=''
+    //}
+}
+histogram([5,3,5],'#')
