4058105417c1bde8c3adf8184d93da7a9e54b8a8
Anusree Melath <anusree450m@gmail.com>
2024-06-26 11:25:24 +0530

    function named range of numbers without using loop

---
+++ js-problems/rangeofnumbers-with -twoparameters/main.js
@@ -0,0 +1,21 @@
+
+let numbersArr=[]
+    const rangeOfNumbers=(startNum,endNum)=>{
+        
+        //for(let i=startNum;i<=endNum;i++){
+          //  numbersArr.push(i)
+       // }
+//return numbersArr
+if(startNum>endNum) return
+if(startNum<=endNum){
+    numbersArr.push(startNum)
+    startNum= startNum + 1
+
+}
+
+rangeOfNumbers(startNum,endNum)
+return numbersArr
+    } 
+ 
+
+console.log(rangeOfNumbers(3,10))
